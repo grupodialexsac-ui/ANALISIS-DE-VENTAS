7ea92420076c8fa155e97a4cc29d5dd0c61f5190
grupodialexsac-ui <grupodialexsac@gmail.com>
2026-05-25 08:30:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 // Módulo principal con encapsulación y mejoras de rendimiento
 (function() {
-
-    // --- URLs codificadas ---
     const urlsEncoded = {
         vendedores: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlE3MEZ1VEY3Y2VySE9RU05YckljTEZERlJwcmZIQVY3MjhDZUtMc21OWmRseHEzckFfU3VuWjZJTHhZRnRaVkhWZlFkcGhVeWNmTmJVQy9wdWI/Z2lkPTAmc2luZ2xlPXRydWUmb3V0cHV0PWNzdg==',
-
         ventas: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlE3MEZ1VEY3Y2VySE9RU05YckljTEZERlJwcmZIQVY3MjhDZUtMc21OWmRseHEzckFfU3VuWjZJTHhZRnRaVkhWZlFkcGhVeWNmTmJVQy9wdWI/Z2lkPTU4ODYyMDUzMSZzaW5nbGU9dHJ1ZSZvdXRwdXQ9Y3N2',
-
         productos: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlE3MEZ1VEY3Y2VySE9RU05YckljTEZERlJwcmZIQVY3MjhDZUtMc21OWmRseHEzckFfU3VuWjZJTHhZRnRaVkhWZlFkcGhVeWNmTmJVQy9wdWI/Z2lkPTEyNDE4OTE1MDMmc2luZ2xlPXRydWUmb3V0cHV0PWNzdg==',
-
         clientes: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlE3MEZ1VEY3Y2VySE9RU05YckljTEZERlJwcmZIQVY3MjhDZUtMc21OWmRseHEzckFfU3VuWjZJTHhZRnRaVkhWZlFkcGhVeWNmTmJVQy9wdWI/Z2lkPTEzNDQ2NDQ2MDgmc2luZ2xlPXRydWUmb3V0cHV0PWNzdg=='
     };
 
