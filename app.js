2d26474ab98716b6bc1cb70ea97a198d30addb5e
grupodialexsac-ui <grupodialexsac@gmail.com>
2026-05-25 08:29:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@
         clientes: 'aHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vc3ByZWFkc2hlZXRzL2QvZS8yUEFDWC0xdlE3MEZ1VEY3Y2VySE9RU05YckljTEZERlJwcmZIQVY3MjhDZUtMc21OWmRseHEzckFfU3VuWjZJTHhZRnRaVkhWZlFkcGhVeWNmTmJVQy9wdWI/Z2lkPTEzNDQ2NDQ2MDgmc2luZ2xlPXRydWUmb3V0cHV0PWNzdg=='
     };
 
+const urls = {
+    vendedores: atob(urlsEncoded.vendedores),
+    ventas: atob(urlsEncoded.ventas),
+    productos: atob(urlsEncoded.productos),
+    clientes: atob(urlsEncoded.clientes)
+};
+
     // --- Estado global interno ---
     let data = {
         vendedoresRaw: [],
